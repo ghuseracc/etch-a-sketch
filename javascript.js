6daefbd28f4ed16ae23c34e4910ce23f1f0d3a01
username <nkuluthuli@gmail.com>
2022-11-19 13:20:05 +0200

    Create 16X16 grid with javascript and css

--- javascript.js
+++ javascript.js
@@ -1,4 +1,17 @@
+const container = document.querySelector('#container')
 
-const container = document.querySelector('#container');
+const content = document.querySelector('.content');
+content.style.outline = '1px solid black';
+content.style.width = '768px'
+content.style.height = '768px';
+let grid = (768/16).toString();
 
-const content = document.querySelector('content');
+for (i = 0 ; i < 16 ; i++) {
+    for (j = 0 ; j < 16 ; j++) {
+    const div = document.createElement('div');
+    div.style.outline = '1px solid black';
+    div.style.width = grid + 'px';
+    div.style.height = grid + 'px';
+    content.appendChild(div);
+    }
+}
